39a3817c8b7f0a17f1b29ab82f864a4b41d79584
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2024-03-29 01:30:44 +0600

    First commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      App
+      <Button>click me</Button>
+    </div>
+  );
+}
