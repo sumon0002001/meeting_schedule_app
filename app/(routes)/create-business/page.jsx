d1bf34f429da7419fa147368ae5ff72f8468dbc6
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2024-03-29 18:34:54 +0600

    Add data succesfull to database

--- app/(routes)/create-business/page.jsx
+++ app/(routes)/create-business/page.jsx
@@ -1,9 +1,63 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { app } from "@/config/FirebaseConfig";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
+import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
+import { useRouter } from "next/navigation";
 
 const CreateBusiness = () => {
+  const [businessName, setBusinessName] = useState("");
+  const db = getFirestore(app);
+  const {user } = useKindeBrowserClient()
+  const router = useRouter()
+
+  const onCreateBusiness = async () => {
+    console.log("clicked", businessName);
+    await setDoc(doc(db, "Business", user.email), {
+      businessName:businessName.replace(" ","_"),
+      email:user.email,
+      userName:user.given_name+" "+user.family_name
+    }).then (Response => {
+      console.log("response", Response)
+      console.log("document saved")
+     
+      router.replace("/dashboard")
+    }
+      
+    )
+    
+
+  };
   return (
     <div className="p-14 items-center flex flex-col gap-20 my-10">
-      createbusiness
+      <Image src="/logo.svg" width={200} height={200} alt="logo" />
+      <div className="flex flex-col items-center gap-4 max-w-3xl">
+        <h2 className="text-4xl font-bold">
+          What should we call your business?
+        </h2>
+        <p className="text-slate-500">
+          You can always change this later from setting
+        </p>
+        <div className="w-full">
+          <label className="text-slate-500">Team Name</label>
+          <Input
+            placeholder="Ex. sumon"
+            className="mt-2"
+            onChange={(event) => setBusinessName(event.target.value)}
+          />
+        </div>
+        <Button
+          className="w-full"
+          disabled={!businessName}
+          onClick={onCreateBusiness}
+        >
+          Create Business
+        </Button>
+      </div>
     </div>
   );
 };
