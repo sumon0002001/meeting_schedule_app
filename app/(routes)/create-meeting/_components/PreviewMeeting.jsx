7ee79143f35071bebf768a2c163911b179b75f04
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2024-04-01 05:39:48 +0600

    Add calender

--- app/(routes)/create-meeting/_components/PreviewMeeting.jsx
+++ app/(routes)/create-meeting/_components/PreviewMeeting.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import { Clock, MapPin } from "lucide-react";
 import Link from "next/link";
 
 import { Calendar } from "@/components/ui/calendar";
+import { Button } from "@/components/ui/button";
 
 const PreviewMeeting = ({ formValue }) => {
+  const [date, setDate] = useState(new Date());
+  const [timeSlots, setTimeSlots] = useState();
+
+  useEffect(() => {
+    formValue?.duration&&createTimeSlot(formValue?.duration);
+  }, [formValue])
+
+
+  const createTimeSlot=(interval)=>{
+    const startTime = 8 * 60; // 8 AM in minutes
+    const endTime = 22 * 60; // 10 PM in minutes
+    const totalSlots = (endTime - startTime) / interval;
+    const slots = Array.from({ length: totalSlots }, (_, i) => {
+  const totalMinutes = startTime + i * interval;
+  const hours = Math.floor(totalMinutes / 60);
+  const minutes = totalMinutes % 60;
+  const formattedHours = hours > 12 ? hours - 12 : hours; // Convert to 12-hour format
+  const period = hours >= 12 ? 'PM' : 'AM';
+  return `${String(formattedHours).padStart(2, '0')}:${String(minutes).padStart(2, '0')} ${period}`;
+});
+
+console.log(slots)
+setTimeSlots(slots); 
+}
+
   return (
     <div className="p-5 py-5 shadow-lg m-5 border-t-8">
       <Image src="/logo.svg" alt="logo" width={100} height={100} />
@@ -41,6 +67,16 @@ const PreviewMeeting = ({ formValue }) => {
         <div className="md:col-span-2 flex px-4">
           <div className="flex flex-col">
             <h2 className="font-bold text-lg">Select Date & Time</h2>
+            <Calendar
+              mode="single"
+              selected={date}
+              onSelect={setDate}
+              className="rounded-md border mt-5"
+              disabled={(date) => date <= new Date()}
+            />
+          </div>
+          <div className="flex flex-col w-full overflow-auto gap-4 p-5">
+            <Button>Slot</Button>
           </div>
         </div>
       </div>
